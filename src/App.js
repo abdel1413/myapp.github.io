1b1adce4498199f117db7a0ef426021ed09a1cdc
abdel <aboualyet63@gmail.com>
2020-11-11 15:48:10 -0500

    added some styles to App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from './Aboulaye.jpeg';
 import './App.css';
+import 'tachyons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className ='name'>
            My name is Aboulaye Tchakoura and I'm going to master React!
         </p>
         <a
-          className="App-link"
+          className="App-link grow"
           href="https://github.com/abdel1413/myapp.github.io.git"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Check Me Out!
         </a>
       </header>
